7d7bde7f63f092e9e84e2a7ed171beab38a8bed1
ATAUL MAOLA ANAS <ataulmaola@users.noreply.github.com>
2018-02-08 19:54:37 +0600

    Ellipse

---
+++ Ellipse.cpp
@@ -0,0 +1,64 @@
+#include<conio.h>
+#include<stdio.h>
+#include<graphics.h>
+#include<math.h>
+void disp();
+float x,y;
+int xc,yc;
+int main()
+{
+int gd=DETECT,gm;
+int a,b;
+float p1,p2;
+
+initgraph(&gd,&gm,"");
+scanf("%d%d",&xc,&yc);
+scanf("%d%d",&a,&b);
+x=0;y=b;
+disp();
+p1=(b*b)-(a*a*b)+(a*a)/4;
+while((2.0*b*b*x)<=(2.0*a*a*y))
+{
+x++;
+if(p1<=0)
+p1=p1+(2.0*b*b*x)+(b*b);
+else
+{
+y--;
+p1=p1+(2.0*b*b*x)+(b*b)-(2.0*a*a*y);
+}
+disp();
+x=-x;
+disp();
+x=-x;
+}
+x=a;
+y=0;
+disp();
+p2=(a*a)+2.0*(b*b*a)+(b*b)/4;
+while((2.0*b*b*x)>(2.0*a*a*y))
+{
+y++;
+if(p2>0)
+p2=p2+(a*a)-(2.0*a*a*y);
+else
+{
+x--;
+p2=p2+(2.0*b*b*x)-(2.0*a*a*y)+(a*a);
+}
+disp();
+y=-y;
+disp();
+y=-y;
+}
+getch();
+closegraph();
+return 0;
+}
+void disp()
+{
+putpixel(xc+x,yc+y,10);
+putpixel(xc-x,yc+y,10);
+putpixel(xc+x,yc-y,10);
+putpixel(xc+x,yc-y,10);
+}
