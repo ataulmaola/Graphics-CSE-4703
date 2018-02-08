9d516168c928a7e5da0de9ec37fb6ea63e7a5bd2
ATAUL MAOLA ANAS <ataulmaola@users.noreply.github.com>
2018-02-08 19:59:05 +0600

    Tangent Curve

---
+++ TanCurve.cpp
@@ -0,0 +1,38 @@
+#include <graphics.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main() {
+    int gd = DETECT, gm;
+    int angle = 0;
+    double x, y;
+
+    initgraph(&gd, &gm, "c:\\turboc3\\bgi");
+
+ line(0, getmaxy() / 2, getmaxx(), getmaxy() / 2);
+ /* generate a sine wave */
+ for(x = 0; x < 360; x+=3) {
+
+     /* calculate y value given x */
+     y = 50*cos(angle*3.141/180);
+
+     y = getmaxy()/2 - y;
+
+     /* color a pixel at the given position */
+  putpixel(x, y, 15);
+
+
+
+  /* increment angle */
+  angle+=5;
+ }
+
+ getch();
+
+ /* deallocate memory allocated for graphics screen */
+ closegraph();
+
+ return 0;
+}
